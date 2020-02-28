a5058a98ee56e460c95fed25f48076e3b0fce345
Maximilien-G <61511257+Maximilien-G@users.noreply.github.com>
2020-02-28 17:45:55 +0100

    Add files via upload

---
+++ exo2.py
@@ -0,0 +1,13 @@
+import os
+os.chdir("C:/Users/maxim/Documents/test python")
+
+
+mon_fichier = open("exo2.txt", "w")
+mon_fichier.write("cocou je suis l'exo2")
+mon_fichier.close()
+
+
+mon_fichier = open("exo2.txt", "r")
+print(contenu)
+mon_fichier.(close)
+
---
+++ exo4.py
@@ -0,0 +1,15 @@
+import os
+os.chdir("C:/Users/maxim/Documents/test python")
+
+def ecriture():
+mon_fichier = open("exo3.txt","w")
+mon_fichier.write("cocou je suis l'exo3")
+mon_fichier.close()
+
+def lire():
+mon_fichier = open("exo3.txt", "r")
+print(contenu)
+mon_fichier.(close)
+
+if __name__ == "__main__":
+main()
