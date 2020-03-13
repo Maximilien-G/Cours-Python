c957edc6875a1b39b7a8afeb0c4bad01e4d824d5
Maximilien-G <61511257+Maximilien-G@users.noreply.github.com>
2020-03-13 16:50:31 +0100

    Add files via upload

---
+++ ferme.py
@@ -0,0 +1,33 @@
+import random
+
+class Animal():
+        def __init__(self,typeAnimal,age,):
+            self.type = typeAnimal
+            self.age = age
+            self.sex =  random.choice(['Femelle', 'Male','Non Binaire'])
+     
+        def __str__(self):
+            return "Type: {} age: {} sex: {}".format(self.type, self.age, self.sex)
+
+class Farm:
+        def __init__(self, nom):
+            self.nom = nom
+            self.Farm_Animal = []
+    
+        def __str__(self):
+            out = ""
+            for my_key in self.Farm_Animal:
+                out += str(my_key) + "\n"
+
+            return out   
+
+if __name__ == "__main__":  
+    Farm_list = []
+    Farm_list.append(Farm("Farm"))
+    Farm_list[0].Farm_Animal.append(Animal("vache", 1))
+    Farm_list[0].Farm_Animal.append(Animal("poule", 1))
+    Farm_list[0].Farm_Animal.append(Animal("mouton", 1))
+
+    print(Farm_list[0])
+     
+  
